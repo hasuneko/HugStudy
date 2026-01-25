a4079e9211ec5583f645bb4712f79d77c41b91b7
hasuneko <sakura03neko65@gmail.com>
2026-01-25 11:33:31 +0900

    Update Task2_23

--- Study/2-23/src/task2_23/Task2_23.java
+++ Study/2-23/src/task2_23/Task2_23.java
@@ -17,8 +17,10 @@ public class Task2_23 {
         //引数には1:山田太郎 2:Java を入れて下さい。
         //また「Java」は、Constants.javaのものを扱って下さい。
 
-        Student yamada = new Student("山田太郎", Constants.LANGUAGE_JAVA);
-
+        Person yamada = new Student("山田太郎", Constants.LANGUAGE_JAVA);
+        // インスタンスをリストに追加
+        persons.add(yamada);
+        
 
         //②作成した変数「yamada」を利用し名前を表示して下さい。
 
@@ -29,18 +31,18 @@ public class Task2_23 {
         //引数には1:石谷花子 2:HTML を入れて下さい。
         //また「HTML」は、Constants.javaのものを扱って下さい。
         
-        Student ishitani = new Student("石谷花子", Constants.LANGUAGE_HTML);
-        
+        Person ishitani = new Student("石谷花子", Constants.LANGUAGE_HTML);
+        // インスタンスをリストに追加
+        persons.add(ishitani);
         
         //④作成した変数「ishitani」を利用し学んでいる言語を表示して下さい。
 
         System.out.println(ishitani.getLanguage());
         
         //⑤「〇〇が△△を学んでいます」という表示で山田さん、石谷さんが何を学んでいるか出力して下さい。
-        
-        yamada.studyLanguage();
-        ishitani.studyLanguage();
-
+        for (Person person : persons) {
+            person.studyLanguage();
+        }
     }
 
 }
